867772044b54ab71c4b7f5b2b3b5c8d13920f7f2
Pete_and_Fuzzy <szepanski@gmail.com>
2018-06-06 19:11:45 +0200

    Initial Commit

---
+++ iosMath Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosMath Test
+//
+//  Created by Peter and Fuzzy on 06.06.18.
+//  Copyright © 2018 Peter and Fuzzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
