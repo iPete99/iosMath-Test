5a0050bb7cf7ef8ed7ae354c9f1431f051a149e9
Pete_and_Fuzzy <szepanski@gmail.com>
2018-06-06 19:25:11 +0200

    initial upload

--- iosMath Test/ViewController.swift
+++ iosMath Test/ViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.testLabel.sizeToFit()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
